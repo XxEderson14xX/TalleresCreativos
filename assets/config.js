c07afb7c59889c8a444bfea4c8139b6fec1cadbe
XxEderson14xX <65546844+XxEderson14xX@users.noreply.github.com>
2026-09-13 22:48:09 -0600

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -6,5 +6,5 @@
 //     "anon public" apikey -> SUPABASE_ANON_KEY
 // Estos valores NO son secretos, están hechos para usarse en el navegador.
 // =====================================================================
-const SUPABASE_URL = "https://TU-PROYECTO.supabase.co";
-const SUPABASE_ANON_KEY = "TU-ANON-KEY-PUBLICA";
+const SUPABASE_URL = "sb_publishable_rdqlu1_pzcbjM-7gnyFQWw_9cvmTkyP";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNuemlkY3N1dHl6aXRnZmh5cXdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkzNTA2OTMsImV4cCI6MjEwNDkyNjY5M30.veEC8UyTKNb7o9n3vZykPKmtNHC6s9Ad3jSHQftEMR8";
